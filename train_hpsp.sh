71f076c67f2ff7a01cc2957c24df1c257a50dc19
IPTransformer <94435336+IPTransformer@users.noreply.github.com>
2022-03-08 11:37:42 +0800

    Update train_hpsp.sh

--- train_hpsp.sh
+++ train_hpsp.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 \
 --master_addr 127.0.1.17 --master_port=20217 \
-train_iptr_cihp.py --opt adam --workers 4 --batch-size 16 \
+train_hpsp_cihp.py --opt adam --workers 4 --batch-size 16 \
 --epochs 150 --loss-type dice \
 --lr 1e-3 \
 --dataset cihp \
